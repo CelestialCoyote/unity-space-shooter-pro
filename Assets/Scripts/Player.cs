21637ed5eb26f2e1501104187c765013fcb553b9
Paul Stearns <celestialcoyote@gmail.com>
2023-03-27 16:54:29 -0700

    Finished Laser prefab

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,44 +8,57 @@ public class Player : MonoBehaviour
 	private float _speed = 3.5f;
 	[SerializeField]
 	private GameObject _laserPrefab;
+	[SerializeField]
+	private float _fireRate = 0.15f;
+	private float _canFire = -1.0f;
 
-    void Start()
-    {
+	void Start()
+	{
 		transform.position = new Vector3(0, 0, 0);
-    }
+	}
 
-    void Update()
-    {
+	void Update()
+	{
 		CalculateMovement();
 
-		if (Input.GetKeyDown(KeyCode.Space))
+		if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
 		{
-			Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+			FireLaser();
 		}
-    }
+	}
 
 	void CalculateMovement()
 	{
 		float horizontalInput = Input.GetAxis("Horizontal");
 		float verticalInput = Input.GetAxis("Vertical");
-		
+
 		Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
 		transform.Translate(direction * _speed * Time.deltaTime);
 
 		if (transform.position.x > 11.3f)
 		{
 			transform.position = new Vector3(-11.3f, transform.position.y, 0);
-		} else if (transform.position.x < -11.3f)
+		}
+		else if (transform.position.x < -11.3f)
 		{
 			transform.position = new Vector3(11.3f, transform.position.y, 0);
 		}
 
-		if (transform.position.y  >= 0)
+		if (transform.position.y >= 0)
 		{
 			transform.position = new Vector3(transform.position.x, 0, 0);
-		} else if (transform.position.y < -3.8f)
+		}
+		else if (transform.position.y < -3.8f)
 		{
 			transform.position = new Vector3(transform.position.x, -3.8f, 0);
 		}
+	}
+
+	void FireLaser()
+	{
+		Vector3 laserOffset = new Vector3(0, 0.8f, 0);
+		
+		_canFire = Time.time + _fireRate;
+		Instantiate(_laserPrefab, transform.position + laserOffset, Quaternion.identity);
 	}
 }
