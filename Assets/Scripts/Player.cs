db63ef4cd087c807dbbb5561787f54babb942ef4
Paul Stearns <celestialcoyote@gmail.com>
2023-03-26 17:21:04 -0700

    Added Player script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	[SerializeField]
+	private float _speed = 3.5f;
+	//public float horizontalInput;
+	//public float verticalInput;
+
+    void Start()
+    {
+		transform.position = new Vector3(0, 0, 0);
+    }
+
+    void Update()
+    {
+		float horizontalInput = Input.GetAxis("Horizontal");
+		float verticalInput = Input.GetAxis("Vertical");
+		
+		Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+		transform.Translate(direction * _speed * Time.deltaTime);
+    }
+}
