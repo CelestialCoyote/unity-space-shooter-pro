f8add2a5d8861595d6e89af62e540f4cfb5bfe7c
Paul Stearns <celestialcoyote@gmail.com>
2023-03-26 19:33:12 -0700

    Adding laser prefab

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 {
 	[SerializeField]
 	private float _speed = 3.5f;
+	[SerializeField]
+	private GameObject _laserPrefab;
 
     void Start()
     {
@@ -14,22 +16,36 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+		CalculateMovement();
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+		}
+    }
+
+	void CalculateMovement()
+	{
 		float horizontalInput = Input.GetAxis("Horizontal");
 		float verticalInput = Input.GetAxis("Vertical");
 		
 		Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
 		transform.Translate(direction * _speed * Time.deltaTime);
 
-		if (transform.position.x > 11.3f) {
+		if (transform.position.x > 11.3f)
+		{
 			transform.position = new Vector3(-11.3f, transform.position.y, 0);
-		} else if (transform.position.x < -11.3f) {
+		} else if (transform.position.x < -11.3f)
+		{
 			transform.position = new Vector3(11.3f, transform.position.y, 0);
 		}
 
-		if (transform.position.y  >= 0) {
+		if (transform.position.y  >= 0)
+		{
 			transform.position = new Vector3(transform.position.x, 0, 0);
-		} else if (transform.position.y < -3.8f) {
+		} else if (transform.position.y < -3.8f)
+		{
 			transform.position = new Vector3(transform.position.x, -3.8f, 0);
 		}
-    }
+	}
 }
